f046be8168ffc71ff73aae808c702b5ec7253885
vitalii-susukailo <80201007+vitalii-susukailo@users.noreply.github.com>
2023-03-27 22:34:47 +0300

    Automated build

---
+++ src/BarberShop.java
@@ -0,0 +1,23 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BarberShop {
+
+    private Queue<Customer> customerQueue = new LinkedList<>();
+
+    public void addCustomer(Customer customer) {
+        customerQueue.add(customer);
+    }
+
+    public void removeCustomer(Customer customer) {
+        customerQueue.remove(customer);
+    }
+
+    public int getCustomerCount() {
+        return customerQueue.size();
+    }
+
+    public Customer getNextCustomer() {
+        return customerQueue.peek();
+    }
+}
